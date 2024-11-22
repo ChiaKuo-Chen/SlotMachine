afc38ec592cee90dae955589dbd0655dd0f5f8e8
Chia-kuo, Chen <tear4701@gmail.com>
2024-11-22 16:28:52 +0800

    Initial Commit

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by 陳嘉國 on 2024/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
